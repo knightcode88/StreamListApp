2f4582bead0e142a62d52a84a8ef0c3bd428f710
knightcode88 <andrea.knight2@student.uagc.edu>
2026-06-13 15:09:54 -0500

    refactor: update App.js to use new component structure and context provider

--- streamlistapp/src/App.js
+++ streamlistapp/src/App.js
@@ -1,43 +1,26 @@
-import { NavLink, Routes, Route } from "react-router-dom";
-import StreamList from "./pages/StreamList";
-import Movies from "./pages/Movies";
-import Cart from "./pages/Cart";
-import About from "./pages/About";
+import { Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import ErrorBoundary from './components/common/ErrorBoundary';
+import Layout from './components/common/Layout';
+import StreamListPage from './pages/StreamList';
+import MoviesPage from './pages/Movies';
+import CartPage from './pages/Cart';
+import AboutPage from './pages/About';
+import './App.css';
 
 export default function App() {
   return (
-    <>
-<nav>
-  <div className="left-links">
-    <NavLink to="/" end>
-      <span className="material-symbols-outlined">queue_music</span>
-      StreamList
-    </NavLink>
-    <NavLink to="/movies">
-      <span className="material-symbols-outlined">movie</span>
-      Movies
-    </NavLink>
-  </div>
-  <div className="right-links">
-    <NavLink to="/cart">
-      <span className="material-symbols-outlined">shopping_cart</span>
-      Cart
-    </NavLink>
-
-    <NavLink to="/about">
-      <span className="material-symbols-outlined">info</span>
-      About
-    </NavLink>
-  </div>
-</nav>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<StreamList />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </>
+    <AppProvider>
+      <ErrorBoundary>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<StreamListPage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </Layout>
+      </ErrorBoundary>
+    </AppProvider>
   );
 }
